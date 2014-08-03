e0957f48f4f9aa0f120cf3f69b39b39cb93708f9
Robin Ricard <ricard.robin@gmail.com>
2014-08-04 01:28:58 +0200

    Better options

--- plugins/etcd/hosts.py
+++ plugins/etcd/hosts.py
@@ -8,26 +8,39 @@ import json
 def last_key_element(key):
     return key.split('/')[-1]
 
-
-host = os.getenv('ETCD_HOST', '127.0.0.1')
+master = json.loads(
+    os.get_env('ETCD_MASTER', '{"ansible_ssh_host":"127.0.0.1"}')
+)
+host = os.getenv('ETCD_HOST', master['ansible_ssh_host'])
 port = os.getenv('ETCD_PORT', 4001)
+ssh_host = os.getenv('SSH_HOST', host)
+ssh_port = os.getenv('SSH_PORT', 22)
 namespace = os.getenv('ETCD_NAMESPACE', 'ansible')
 
 client = etcd.Client(host=host, port=port)
 
-output = {'_meta': {'hostvars': {}}}
+output = {
+    '_meta': {
+        'hostvars': {
+            'etcd_master': master
+        }
+    }
+}
 
-groups = client.node.get('/%s/groups' % (namespace), recursive=True)
+try:
+    groups = client.node.get('/%s/groups' % (namespace), recursive=True)
 
-for group in groups.node.children:
-    group_name = last_key_element(group.key)
-    if group.is_directory:
-        output[group_name] = {'hosts': []}
-        for host in group.children:
-            host_name = last_key_element(host.key)
-            output['_meta']['hostvars'][host_name] = json.loads(host.value)
-            output[group_name]['hosts'].append(host_name)
-    else:
-        output[group_name] = json.loads(group.value)
+    for group in groups.node.children:
+        group_name = last_key_element(group.key)
+        if group.is_directory:
+            output[group_name] = {'hosts': []}
+            for host in group.children:
+                host_name = last_key_element(host.key)
+                output['_meta']['hostvars'][host_name] = json.loads(host.value)
+                output[group_name]['hosts'].append(host_name)
+        else:
+            output[group_name] = json.loads(group.value)
+except KeyError:
+    pass
 
 print json.dumps(output)
